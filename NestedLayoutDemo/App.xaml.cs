8060af4acb009096bbe71b28878b08fe09d64b26
michael Harrison <harrison464@gmail.com>
2021-06-20 00:23:09 -0400

    first push to the repo

---
+++ NestedLayoutDemo/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using NestedLayoutDemo.ViewModels;
+using NestedLayoutDemo.Views;
+using Prism.Ioc;
+using Xamarin.Forms;
+
+namespace NestedLayoutDemo
+{
+    public partial class App
+    {
+        public App()
+        {
+        }
+
+        protected override async void OnInitialized()
+        {
+            InitializeComponent();
+
+            var result = await NavigationService.NavigateAsync("/NavigationPage/MainPage");
+
+            if (!result.Success)
+            {
+                System.Diagnostics.Debugger.Break();
+            }
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<MainPage, MainPageViewModel>();
+        }
+    }
+}
---
+++ NestedLayoutDemo/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Prism.Navigation;
+
+namespace NestedLayoutDemo.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            OrderItemsList = new ObservableCollection<Element>();
+            CreateLists();
+        }
+
+        ObservableCollection<Element> _OrderItemsList;
+        public ObservableCollection<Element> OrderItemsList
+        {
+            get { return _OrderItemsList; }
+            set { SetProperty(ref _OrderItemsList, value); }
+        }
+
+        private void CreateLists()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                var hasModList = false;
+                if (i % 2 == 0)
+                {
+                    hasModList = true;
+                }
+
+                OrderItemsList.Add(new Element
+                {
+                    Name = $"item{i}",
+                    Price = 0,
+                    ItemCost = "0.0",
+                    HasExtras = hasModList,
+                });
+                if (hasModList)
+                {
+                    for (int j = 0; j < 3; j++)
+                    {
+                        ObservableCollection<ModificationsElement> mods = new ObservableCollection<ModificationsElement>
+                        {
+                            new ModificationsElement
+                            {
+                                Id = $"id{j}",
+                                Name = $"item{j}",
+                                Price = j.ToString(),
+                                Amount = 0
+                            }
+                        };
+                    }
+                }
+            }
+        }
+    }
+
+    public class Element
+    {
+        public string Name { get; set; }
+
+        public long? Price { get; set; }
+
+        public Modifications Modifications { get; set; }
+
+        public string ItemCost { get; set; }
+
+        public bool HasExtras { get; set; }
+
+        public ObservableCollection<ModificationsElement> Mods { get; set; }
+    }
+
+    public partial class Modifications
+    {
+        public ObservableCollection<ModificationsElement> Elements { get; set; }
+    }
+
+    public partial class ModificationsElement
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public long? Amount { get; set; }
+
+        public string Price { get; set; }
+    }
+}
